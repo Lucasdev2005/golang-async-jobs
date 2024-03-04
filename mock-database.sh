70f0091a832437eb09039c58f2b4b976be3e1094
Lucas Moreira Nunes <lucas2005dev@gmail.com>
2024-03-04 11:46:58 -0300

    Update mock-database.sh

--- mock-database.sh
+++
@@ -1,7 +0,0 @@
-docker run -d \
-    --name asyncjobs-postgres \
-    -e POSTGRES_PASSWORD=asyncjobs@123 \
-    -e POSTGRES_USER=asyncjobs \
-    -e POSTGRES_DB=asyncjobs \
-    -p 5432:5432 \
-    postgres:latest
