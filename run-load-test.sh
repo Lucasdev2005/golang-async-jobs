929eaa6d53b9f854b32a24ad1249147bb917b198
Lucas Moreira Nunes <lucas2005dev@gmail.com>
2024-03-25 14:32:48 -0300

    Update README.md and run-load-test.sh

--- run-load-test.sh
+++ run-load-test.sh
@@ -1,3 +1,4 @@
+docker compose up -d
 echo "--------------------------------------------------------------------------------------"
 echo "Load testing with Grafana dashboard http://localhost:3000/d/k6/k6-load-testing-results"
 echo "--------------------------------------------------------------------------------------"
